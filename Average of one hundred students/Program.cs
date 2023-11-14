413d26ae6080e9138755f4981b67e426756cc644
Acer <Acer@DESKTOP-JS0O9TD>
2023-11-15 03:29:03 +0330

    Add project files.

---
+++ Average of one hundred students/Program.cs
@@ -0,0 +1,150 @@
+﻿using Average_of_one_hundred_students;
+using System.Xml.Linq;
+
+List<Student> students = new List<Student>();
+
+StreamReader read = new StreamReader(@"C:\Users\Acer\Desktop\c#\Average of one hundred students\num.txt");
+
+while (!read.EndOfStream)
+{
+
+    string line = read.ReadLine();
+    Student student = new Student();
+    student.Fname = line.Split(',')[0];
+    student.Lname = line.Split(",")[0];
+    student.pishrafte1 = Convert.ToDouble(line.Split(',')[1]);
+    student.tafsir = Convert.ToDouble(line.Split(',')[2]);
+    student.riazi = Convert.ToDouble(line.Split(',')[3]);
+    student.kargah = Convert.ToDouble(line.Split(',')[4]);
+    student.zaban = Convert.ToDouble(line.Split(',')[5]);
+    student.pishrafte2 = Convert.ToDouble(line.Split(',')[6]);
+    student.shay_garai = Convert.ToDouble(line.Split(',')[7]);
+    student.tarbiat_badani = Convert.ToDouble(line.Split(',')[8]);
+    student.system_amle = Convert.ToDouble(line.Split(',')[9]);
+    student.Algorithm = Convert.ToDouble(line.Split(',')[10]);
+
+
+    students.Add(student);
+
+}
+bool exit = false;
+do
+{
+    Student student = new Student();
+
+    student.write();
+    int choice = Convert.ToInt32(Console.ReadLine());
+    //1.kol moadel ha
+    if (choice == 1)
+    {
+        foreach (var item in students)
+        {
+            student.list_kol(item.Fname, item.Lname, item.totalGPA(), item.omomi(), item.nime_takhasosi(), item.takhsosi());
+            Console.WriteLine("=======================================================================");
+        }
+
+    }
+    //2.moadel kol
+    else if (choice == 2)
+    {
+        string[]fname=new string[100];
+        string[] lname = new string[100];
+        double[] kol = new double[100];
+        int index = 0; // a counter for the arrays
+        foreach (var item in students)
+        {
+            fname[index] = item.Fname;
+            lname[index] = item.Lname;
+            kol[index] = item.totalGPA();
+            index++;
+           
+        }
+        student.list(fname, lname, kol);
+        
+    }
+    //3.moadel omomi
+    else if (choice == 3)
+    {
+        string[] fname = new string[100];
+        string[] lname = new string[100];
+        double[] omomi = new double[100];
+        int index = 0; // a counter for the arrays
+        foreach (var item in students)
+        {
+            fname[index] = item.Fname;
+            lname[index] = item.Lname;
+            omomi[index] = item.omomi();
+            index++;
+
+        }
+        student.list(fname, lname, omomi);
+    }
+    //4.moadel nime takhsosi
+    else if (choice == 4)
+    {
+        string[] fname = new string[100];
+        string[] lname = new string[100];
+        double[] nime = new double[100];
+        int index = 0; // a counter for the arrays
+        foreach (var item in students)
+        {
+            fname[index] = item.Fname;
+            lname[index] = item.Lname;
+            nime[index] = item.nime_takhasosi();
+            index++;
+
+        }
+        student.list(fname, lname, nime);
+    }
+    //5.moadel takhsosi
+    else if (choice == 5)
+    {
+        string[] fname = new string[100];
+        string[] lname = new string[100];
+        double[] takhasosi = new double[100];
+        int index = 0; // a counter for the arrays
+        foreach (var item in students)
+        {
+            fname[index] = item.Fname;
+            lname[index] = item.Lname;
+            takhasosi[index] = item.takhsosi();
+            index++;
+
+        }
+        student.list(fname, lname, takhasosi);
+    }
+    //6.grade daneshjo
+    else if (choice == 6)
+    {
+        foreach (var item in students)
+        {
+            Console.WriteLine(item.Fname + "  " + item.Lname);
+            Console.WriteLine("grad kol:  " + item.grade(item.totalGPA()));
+            Console.WriteLine("grad omomi:  " + item.grade(item.omomi()));
+            Console.WriteLine("grad nime takhsosi:  " + item.grade(item.nime_takhasosi()));
+            Console.WriteLine("grad takhsosi:  " + item.grade(item.takhsosi()));
+            Console.WriteLine("-------------------");
+        }
+    }
+    else if (choice == 7)
+    {
+        foreach (var item in students)
+        {
+            item.adadaval(Convert.ToInt32(item.pishrafte1));
+           
+            item.adadaval(Convert.ToInt32(item.pishrafte2));
+            item.adadaval(Convert.ToInt32(item.tarbiat_badani));
+            item.adadaval(Convert.ToInt32(item.tafsir));
+            item.adadaval(Convert.ToInt32(item.riazi));
+            item.adadaval(Convert.ToInt32(item.shay_garai));
+            item.adadaval(Convert.ToInt32(item.system_amle));
+            item.adadaval(Convert.ToInt32(item.Algorithm));
+            item.adadaval(Convert.ToInt32(item.zaban));
+            item.adadaval(Convert.ToInt32(item.kargah));
+        }
+    }
+    else if (choice == 8)
+    {
+        exit = true;
+    }
+} while (!exit);
---
+++ Average of one hundred students/Student.cs
@@ -0,0 +1,140 @@
+﻿namespace Average_of_one_hundred_students
+{
+    class Student
+    {
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public double pishrafte1 { get; set; }
+        public double tafsir { get; set; }
+        public double riazi { get; set; }
+        public double kargah { get; set; }
+        public double zaban { get; set; }
+        public double pishrafte2 { get; set; }
+        public double shay_garai { get; set; }
+        public double tarbiat_badani { get; set; }
+        public double system_amle { get; set; }
+        public double Algorithm { get; set; }
+
+        public double totalGPA()
+        {
+
+            double result = ((pishrafte1 * 3) + (tafsir * 1) + (riazi * 2) + (kargah * 1) + (zaban * 2) + (pishrafte2 * 3) + (shay_garai * 3) + (tarbiat_badani * 1) + (system_amle * 3) + (Algorithm * 3))
+                 / 22;
+            return result;
+
+        }
+        public double takhsosi()
+        {
+            double result = ((pishrafte1 * 3) + (pishrafte2 * 3) + (shay_garai * 3)) / 9;
+            return result;
+        }
+        public double nime_takhasosi()
+        {
+            double result = ((system_amle * 3) + (Algorithm * 3)) / 6;
+            return result;
+        }
+        public double omomi()
+        {
+            double rerult = ((tafsir * 1) + (riazi * 2) + (kargah * 1) + (zaban * 2) + (tarbiat_badani * 1)) / 7;
+            return rerult;
+        }
+
+        public void write()
+        {
+            Console.WriteLine("Choose one of the following(enter number):");
+            Console.WriteLine("1.kol moadel ha");
+            Console.WriteLine("2.moadel kol");
+            Console.WriteLine("3.moadel omomi");
+            Console.WriteLine("4.moadel nime takhsosi");
+            Console.WriteLine("5.moadel takhsosi");
+            Console.WriteLine("6.grade daneshjo");
+            Console.WriteLine("7.adad aval nomarat");
+            Console.WriteLine("8.exit");
+        }
+
+        public int read()
+        {
+            int choice = Convert.ToInt32(Console.ReadLine());
+            return choice;
+        }
+        public void list_kol(string fname, string lname, double x1, double x2, double x3, double x4)
+        {
+            List<(string, string, double, double, double, double)> ScoreList = new List<(string, string, double, double, double, double)>();
+
+
+            ScoreList.Add((fname, lname, x1, x2, x3, x4));
+
+
+            foreach (var tuple in ScoreList)
+            {
+                Console.WriteLine(tuple.Item1 + "  " + tuple.Item2 + ": ");
+                Console.WriteLine("kol : " + tuple.Item3.ToString("F2") + " | " + "omomi  " + tuple.Item4.ToString("F2") + " | " + "nime takhsosi  " + tuple.Item5.ToString("F2") + " | " + "takhasosi  " + tuple.Item6.ToString("F2"));
+            }
+        }
+        List<(string, string, double)> moadel = new List<(string, string, double)>();
+        public void list(string[] fname, string[] lname, double[] gpa)
+        {
+
+
+            for (int i = 0; i < fname.Length; i++)
+            {
+                moadel.Add((fname[i], lname[i], gpa[i]));
+            }
+
+            moadel.Sort((x, y) => x.Item3.CompareTo(y.Item3));
+            foreach (var tuple in moadel)
+            {
+                Console.WriteLine(tuple.Item1 + " " + tuple.Item2);
+                Console.WriteLine("moadl:  " + tuple.Item3.ToString("F2"));
+                Console.WriteLine("-----------------------");
+            }
+        }
+        public void adadaval(int num)
+        {
+            int sum = 0;
+
+            for (int i = 1; i < num; i++)
+            {
+
+                if (num % i == 0)
+                {
+                    sum++;
+                }
+
+            }
+            if (sum < 2 && num > 1)
+                Console.WriteLine(num);
+        }
+
+        public string grade(double gpa)
+        {
+            switch (gpa)
+            {
+                case double n when (n >= 17 && n <= 20):
+                    return "A";
+                    break;
+                case double n when (n >= 15 && n < 17):
+                    return "B";
+                    break;
+                case double n when (n >= 13 && n < 15):
+                    return "C";
+                    break;
+                case double n when (n >= 10 && n < 13):
+                    return "D";
+                    break;
+                case double n when (n >= 7 && n < 10):
+                    return "E";
+                    break;
+                case double n when (n >= 3 && n < 7):
+                    return "F";
+                    break;
+                case double n when (n >= 0 && n < 3):
+                    return "G";
+                    break;
+                default: return "out of rang";
+
+            }
+        }
+    }
+}
+
